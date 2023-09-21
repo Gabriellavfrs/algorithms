983daeb3fba860e13d0d1e39bea3cfbb5d0793ea
Gabriella Vieira <gabvi2015@outlook.com>
2023-09-20 21:05:51 -0300

    feat: req 1 - student schedule search algorithm

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+    if not target_time:
+        return None
+    for login, logout in permanence_period:
+        if type(login) != int or type(logout) != int:
+            return None
+        if login <= target_time and logout >= target_time:
+            counter += 1
+    return counter
