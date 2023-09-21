2011d671f9620ed84babee6fb87fcec8abc99669
Gabriella Vieira <gabvi2015@outlook.com>
2023-09-21 14:37:50 -0300

    feat: req 3 - recursive algorithm

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    if low_index == high_index or low_index == high_index - 1:
+        return word[low_index] == word[high_index]
+    else:
+        isEqual = word[low_index] == word[high_index]
+        return isEqual and is_palindrome_recursive(
+            word, low_index + 1, high_index - 1)
