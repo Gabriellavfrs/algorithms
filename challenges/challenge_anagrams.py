bb14a1afb1d07c367610628f4f9cc1f7de9d9f66
Gabriella Vieira <gabvi2015@outlook.com>
2023-09-24 12:07:37 -0300

    feat: req 5 - search algorithm

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -8,7 +8,7 @@ def is_anagram(first_string, second_string):
 
     first_sorted = ''.join(first_list)
     second_sorted = ''.join(second_list)
- 
+
     if not first_string or not second_string:
         return (first_sorted, second_sorted, False)
 
@@ -45,33 +45,3 @@ def merge(letters, start, mid, end):
         else:
             letters[general_index] = right[right_index]
             right_index += 1
-
-# def merge_sort(numbers, start=0, end=None):
-#     if end is None:
-#         end = len(numbers)
-#     if (end - start) > 1:
-#         mid = (start + end) // 2
-#         merge_sort(numbers, start, mid)
-#         merge_sort(numbers, mid, end)
-#         merge(numbers, start, mid, end)
-
-
-# def merge(numbers, start, mid, end):
-#     left = numbers[start:mid]
-#     right = numbers[mid:end]
-
-#     left_index, right_index = 0, 0
-
-#     for general_index in range(start, end): 
-#         if left_index >= len(left): 
-#             numbers[general_index] = right[right_index]
-#             right_index = right_index + 1
-#         elif right_index >= len(right): 
-#             numbers[general_index] = left[left_index]
-#             left_index = left_index + 1
-#         elif left[left_index] < right[right_index]: 
-#             numbers[general_index] = left[left_index]
-#             left_index = left_index + 1
-#         else:
-#             numbers[general_index] = right[right_index]
-#             right_index = right_index + 1
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,33 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or len(nums) < 2:
+        return False
+
+    nums.sort()
+
+    for index in range(len(nums) - 1):
+        num = nums[index]
+        if type(num) == str or num < 0:
+            return False
+
+        result = binary_search(nums, num, index + 1)
+        if result:
+            return nums[result]
+    return False
+
+
+def binary_search(numbers, target, start):
+    end = len(numbers) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if numbers[mid] == target:
+            return mid
+
+        if target < numbers[mid]:
+            end = mid - 1
+
+        else:
+            start = mid + 1
+
+    return False
