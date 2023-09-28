b30a453e5273cb7acddcf0277bcf547af781a004
Gabriella Vieira <gabvi2015@outlook.com>
2023-09-28 08:34:57 -0300

    feat: req 6 - iterative algorithm

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,11 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    middle = len(word) // 2
+    low_index = len(word) - 1
+    for high_index in range(middle):
+        if word[high_index] != word[low_index - high_index]:
+            return False
+
+    return True
